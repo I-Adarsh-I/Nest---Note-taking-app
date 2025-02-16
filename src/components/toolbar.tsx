9a2526120673effe9f399bf8ef7efc1ee94a4b1c
Adarsh-Singh-0 <adarshsingh9479020444@gmail.com>
2025-02-16 19:53:05 +0530

    Deployment

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -89,10 +89,9 @@ const Toolbar = ({ initialData, preview }: ToolbarProps) => {
       )}
       <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
         {!initialData.icon && !preview && (
-          <IconPicker asChild onChange={iconSelectHandler}>
+          <IconPicker onChange={iconSelectHandler}>
           <span>
             <Button className="text-muted-foreground text-xs" variant="outline" size="sm" >
-            {/* variant="outline" size="sm" */}
               <Smile className="h-4 w-4 mr-2" />
               Add icon
             </Button>
