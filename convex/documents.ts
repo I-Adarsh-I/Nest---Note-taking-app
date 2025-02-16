04bfc4e78a38db86e01f64a01b3e723755877b8b
Adarsh-Singh-0 <adarshsingh9479020444@gmail.com>
2025-02-16 20:02:46 +0530

    Prod bug resolved

--- convex/documents.ts
+++ convex/documents.ts
@@ -245,7 +245,7 @@ export const getSearch = query({
     }
     const userId = identity.subject;
 
-    const documents = ctx.db
+    const documents = await ctx.db
       .query("documents")
       .withIndex("by_user", (q) => q.eq("userId", userId))
       .filter((q) => q.eq(q.field("isArchived"), false))
