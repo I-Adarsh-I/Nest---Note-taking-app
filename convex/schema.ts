937814b1a6d31e35a8b95b313544ffa8a8bc85c1
Adarsh-Singh-0 <adarshsingh9479020444@gmail.com>
2025-02-18 16:54:14 +0530

    Added new schema

---
+++ convex/schema.ts
@@ -0,0 +1,38 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    //Table for documents
+    documents: defineTable({
+        title: v.string(),
+        userId: v.string(),
+        isArchived: v.boolean(),
+        parentDocument: v.optional(v.id("documents")),
+        content: v.optional(v.string()),
+        coverImage: v.optional(v.string()),
+        icon: v.optional(v.string()),
+        isPublished: v.boolean(),
+        prompt: v.optional(v.string())
+    })
+    .index("by_user",["userId"])
+    .index("by_user_parent",["userId", "parentDocument"]),
+
+    //Table for AI chat messages
+    messags: defineTable({
+        sessionId: v.id("sessions"),
+        userId: v.string(),
+        prompt: v.optional(v.string()),
+        role: v.string(), // to set based on AI and user
+        timestamp: v.string(),
+    }),
+
+    //Table for storing chat sessions
+    sessions: defineTable({
+        userId: v.string(),
+        sessionStart: v.string(),
+        sessionEnd: v.optional(v.string()),
+        isActive: v.boolean(),
+    })
+    .index("by_user", ["userId"])
+    .index("by_active_session", ["userId", "isActive"])
+})
