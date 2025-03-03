7d6581551ef3e03505e98e286aef81fea3a176b3
Adarsh-Singh-0 <adarshsingh9479020444@gmail.com>
2025-03-03 23:46:55 +0530

    Updated use-greet hook

--- src/hooks/use-greet.tsx
+++ src/hooks/use-greet.tsx
@@ -1,8 +1,8 @@
 export const useGreet = () => {
-  let greet = "";
   const myDate = new Date();
   const hrs = myDate.getHours();
-  if (hrs < 12) return (greet = "Good Morning");
-  else if (hrs >= 12 && hrs <= 17) return (greet = "Good Afternoon");
-  else if (hrs >= 17 && hrs <= 24) return (greet = "Good Evening");
+
+  if (hrs < 12) return "Good Morning";
+  if (hrs >= 12 && hrs <= 17) return "Good Afternoon";
+  return "Good Evening";
 };
